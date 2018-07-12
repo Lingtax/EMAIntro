12d44d3f8ea1619db0248a929357108635d662b9
Mathew Ling <m.ling@deakin.edu.au>
2018-07-12 23:24:11 +1000

    Add simulation code and Data from 1 run.

---
+++ simulator.R
@@ -0,0 +1,62 @@
+# ESM data simulator
+# This will generate a simple random data set with 1 between (x) and 1 within (xi) cluster IV
+# there will also be one within subjects DV (y) and a cluster ID (id)
+
+# load packages
+library(tidyverse)
+
+# add helper function
+capper <- function(x) {ifelse(x>10, 10, x)}
+
+# set dataframe scale
+nclust <- 30 # number of clusters / individuals
+nobs <- 30 #number of observations per cluster 
+
+# set random seed
+# seed <- 1 # replace with value to replicate sample
+seed <- runif(1, -9999999, 9999999)
+set.seed(seed)
+
+# defines between subjects parameters a and b; constant c; &  between cluster variable x
+parameters <-  tibble(id = stringi::stri_rand_strings(nclust, 8),
+                      a = rnorm(nclust, rnorm(1, 0, 5), abs(rnorm(10, 0, .2))),
+                      x = rnorm(nclust, rnorm(1, 5, .5), abs(rnorm(10, 0, .8))),  
+                      b = rnorm(nclust, rnorm(1, 0, 5), abs(rnorm(10, 0, .2))),
+                      c = rnorm(nclust, rnorm(1, 0, 5), abs(rnorm(10, 0, .8)))
+)
+
+# makes the data long
+out <- parameters[rep(seq_len(nrow(parameters)), nobs), ]
+
+# adds within cluster parameters
+out <-  out %>% mutate(
+  xi = rnorm(nrow(out), 4, 2.1),
+  error = rnorm(nrow(out), mean = 0, sd = 3.8), # generates error
+  y = (a * x) + (b * xi) + c + error # produces outcome according to function y= ax + bxi + c + error
+  )
+
+# outputs two dataframes to reflect native output structure from instant survey and qualtrics (or equiv)
+
+between <- select(parameters, id, x) %>%
+  distinct(id, .keep_all = TRUE) %>% 
+  mutate(item1 = 11 - capper(round(x + rnorm(nclust, 0, .4), 0)),
+         item2 = capper(round(x + rnorm(nclust, 0, .4), 0)), 
+         item3 = capper(round(x + rnorm(nclust, 0, .4), 0)), 
+         item4 = capper(round(x + rnorm(nclust, 0, .4), 0)) 
+         ) %>% 
+  select(-x)
+
+within <- out %>%  select(id, xi, y)
+
+write_csv(between, paste("baseline.csv", sep=""))
+write_csv(within, paste("esm.csv", sep=""))
+
+# trims parameter frame for recording
+params <-  select(parameters, id, a, b, c)
+
+# records "true" parameters for model diagnostics
+sink(paste(seed,  "_SIMparameters.txt", sep = ""))
+paste("Simulated data parameters from random seed ", seed, sep = "")
+paste("Data contains ", nobs, " observations from each of ", nclust,  " clusters", sep = "")
+print(params, n=Inf)
+sink()
